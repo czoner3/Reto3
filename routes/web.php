dc922687179b69d08d89172b07a7f644740daec1
Julen <julen.ortizdezarate@ikasle.egibide.org>
2020-01-17 13:02:09 +0100

    arreglos varios

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/create/incidencia', 'IncidenciaController@create');
